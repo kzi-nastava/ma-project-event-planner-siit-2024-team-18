e2ddb3f3bc0fd72fc19b7d5def92aa8bd8bbacb2
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2024-11-04 20:23:16 +0100

    [ADD] Splash Screen

--- EventPlanner/app/src/main/java/com/example/eventplanner/MainActivity.java
+++ EventPlanner/app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -2,23 +2,28 @@ package com.example.eventplanner;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // getSupportActionBar().hide();
+        int SPLASH_TIME_OUT = 5000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                //startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
